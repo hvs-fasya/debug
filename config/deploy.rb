9933443a8fd1317b9b383d243f82f91e8e6b24c0
Elena Tolkacheva <hvs-fasya@mail.ru>
2017-10-04 17:34:03 +0300

    smpt settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/deployer/apps/debug"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/smpt.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
